7ccef050df8e40b4c9dc384dfc86a392af282d51
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-03-05 12:34:44 +0900

    bug fix

--- src/DragItem.js
+++ src/DragItem.js
@@ -1,73 +1,54 @@
-import React, { Component } from "react";
-import { DragSource, DropTarget } from "react-dnd";
+import React, { Component } from 'react';
+import { DragSource, DropTarget } from 'react-dnd'
 
-const dragSource = DragSource(
-  "item",
-  {
-    beginDrag(props) {
-      return {
-          id: props.id,
-          name: props.name,
-          originalIndex: props.findItem(props.id),
-      };
-  },
-
-  endDrag(props, monitor) {
-    const { id, originalIndex } = monitor.getItem()
-    const didDrop = monitor.didDrop()
-
-    if (!didDrop) {
-        props.onDrop(id, originalIndex)
-    }
-  },
-}, (connect, monitor) => {
-    return {
-      connectDragSource: connect.dragSource(),
-      connectDragPreview: connect.dragPreview(),
-      isDragging: monitor.isDragging()
-    };
-});
-
-const dropTarget = DropTarget("item", {
-    canDrop() {
-        return false
-    },
-    hover(props, monitor) {
-      const draggedId = monitor.getItem().id
-      const overId = props.id
-
-      if (draggedId !== overId) {
-        const overIndex = props.findItem(overId)
-        props.onDrop(draggedId, overIndex)
-      }
-    },
-    
-  },connect => {
-    return {
-      connectDropTarget: connect.dropTarget()
-    };
+// ドラッグされるSourceの動作を定義する
+const dragSource = DragSource("item", {
+  beginDrag(props) {
+    return props;
   }
-);
+}, (connect, monitor) => {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  };
+})
+
+// ドロップされるTargetの動作を定義する
+const dropTarget = DropTarget("item", {
+  drop(dropProps, monitor) {
+    const dragProps = monitor.getItem();　
+    if (dropProps.id !== dragProps.id) {
+      dragProps.onDrop(dragProps.id, dropProps.id);
+    }
+  }
+}, connect => {
+  return {
+    connectDropTarget: connect.dropTarget()
+  };
+})
 
 class DragItem extends Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
 
   getItemStyles() {
-    const { isDragging } = this.props;
+    const { isDragging } = this.props
 
     return {
-      opacity: isDragging ? 0.4 : 1
-    };
+      opacity: isDragging ? 0.4 : 1,
+    }
   }
 
   render() {
     return this.props.connectDragSource(
       this.props.connectDropTarget(
-        <div style={this.getItemStyles()}>{this.props.name}</div>
+        <div style={this.getItemStyles()}>
+          {this.props.name}
+        </div>
       )
-    );
+    )
   }
 }
 
